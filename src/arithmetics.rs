d8920e372ee97290daa0f5a9fc6a2f34513c1e36
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2022-01-26 20:35:21 +0900

    Implement base fraction operation

---
+++ src/arithmetics.rs
@@ -0,0 +1,166 @@
+use std::{
+    cmp::Ordering,
+    ops::{Add, Mul, Shr},
+};
+
+use num::Num;
+
+pub struct Farey<T> {
+    pub num: T,
+    pub den: T,
+}
+
+impl<T> Clone for Farey<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            num: self.num.clone(),
+            den: self.den.clone(),
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.num = source.num.clone();
+        self.den = source.den.clone();
+    }
+}
+
+impl<T> PartialEq for Farey<T>
+where
+    T: Num + Clone,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.den == other.den && self.num == other.num
+    }
+}
+
+impl<T> PartialOrd for Farey<T>
+where
+    T: Num + Clone + Ord,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let lhs = self.num.clone() * other.den.clone();
+        let rhs = other.num.clone() * self.den.clone();
+        Some(lhs.cmp(&rhs))
+    }
+}
+
+fn push<T>(from: &Farey<T>, to: &Farey<T>, by: &T) -> Farey<T>
+where
+    T: Clone + Mul<Output = T> + Add<Output = T>,
+{
+    Farey {
+        num: from.num.clone() + to.num.clone() * by.clone(),
+        den: from.den.clone() + to.den.clone() * by.clone(),
+    }
+}
+
+fn is_beyond<T>(f1: &Farey<T>, f2: &Farey<T>, m: &T) -> bool
+where
+    T: Clone + Add<Output = T> + PartialOrd,
+{
+    f1.den.clone() + f2.den.clone() > *m
+}
+
+fn approx_right<T>(f1: &Farey<T>, f: &Farey<T>, f2: &Farey<T>, m: &T, allow_equal: bool) -> Farey<T>
+where
+    T: Clone + Num + Ord + PartialOrd + Shr<Output = T>,
+{
+    if is_beyond(f1, f2, m) {
+        return Farey {
+            num: f2.num.clone(),
+            den: f2.den.clone(),
+        };
+    }
+
+    let (mut l, mut r): (T, T) = (
+        T::zero(),
+        (m.clone() - f2.den.clone() + f1.den.clone()) / f1.den.clone(),
+    );
+
+    while l.clone() + T::one() < r {
+        let c = (l.clone() + r.clone()).shr(T::one());
+        let m = push(&f2, f1, &c);
+
+        match f.partial_cmp(&m) {
+            Some(Ordering::Less) => {
+                l = c;
+            }
+            Some(Ordering::Equal) => {
+                if allow_equal {
+                    l = c;
+                } else {
+                    r = c;
+                }
+            }
+            Some(Ordering::Greater) => {
+                r = c;
+            }
+            None => {
+                panic!("Error with comparing two Fareys while performing binary search");
+            }
+        }
+    }
+
+    push(f2, f1, &l)
+}
+
+fn approx_left<T>(f1: &Farey<T>, f: &Farey<T>, f2: &Farey<T>, m: &T, allow_equal: bool) -> Farey<T>
+where
+    T: Clone + Num + Ord + PartialOrd + Shr<Output = T>,
+{
+    if is_beyond(f1, f2, m) {
+        return Farey {
+            num: f1.num.clone(),
+            den: f1.den.clone(),
+        };
+    }
+
+    let (mut l, mut r): (T, T) = (
+        T::zero(),
+        (m.clone() - f1.den.clone() + f2.den.clone()) / f2.den.clone(),
+    );
+
+    while l.clone() + T::one() < r {
+        let c = (l.clone() + r.clone()).shr(T::one());
+        let m = push(&f1, f2, &c);
+
+        match f.partial_cmp(&m) {
+            Some(Ordering::Less) => {
+                r = c;
+            }
+            Some(Ordering::Equal) => {
+                if allow_equal {
+                    l = c;
+                } else {
+                    r = c;
+                }
+            }
+            Some(Ordering::Greater) => {
+                l = c;
+            }
+            None => {
+                panic!("Error with comparing two Fareys while performing binary search");
+            }
+        }
+    }
+
+    push(f1, f2, &l)
+}
+
+pub fn squeeze_right_first<T>(f1: &mut Farey<T>, f: &Farey<T>, f2: &mut Farey<T>, m: &T)
+where
+    T: Clone + Num + Ord + PartialOrd + Shr<Output = T>,
+{
+    loop {
+        let nf2 = approx_right(f1, f, f2, m, true);
+        let nf1 = approx_left(f1, f, &nf2, m, true);
+        if f1 == &nf1 && f2 == &nf2 {
+            break;
+        }
+        f1.clone_from(&nf1);
+        f2.clone_from(&nf2);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,43 @@
+mod arithmetics;
 
+use std::ops::{DivAssign, Rem, Shr};
+
+use arithmetics::*;
+use num::Num;
+
+fn gcd<T>(x: T, y: T) -> T
+where
+    T: Num + Clone + PartialEq + Rem<Output = T>,
+{
+    let (mut a, mut b) = (x.clone(), y.clone());
+    while b != T::zero() {
+        let t = b.clone();
+        b = a % b;
+        a = t.clone();
+    }
+    a
+}
+
+pub fn get_closest_fraction<T>(num: &T, den: &T, max_den: &T) -> (Farey<T>, Farey<T>)
+where
+    T: Clone + Num + Ord + PartialOrd + Shr<Output = T> + DivAssign,
+{
+    let (mut x, mut y) = (num.clone(), den.clone());
+    let g = gcd(x.clone(), y.clone());
+    x /= g.clone();
+    y /= g.clone();
+    let mut f1 = Farey {
+        num: T::zero(),
+        den: T::one(),
+    };
+    let mut f2 = Farey {
+        num: T::one(),
+        den: T::one(),
+    };
+    let f = Farey {
+        num: x.clone(),
+        den: y.clone(),
+    };
+    squeeze_right_first(&mut f1, &f, &mut f2, max_den);
+    (f1, f2)
+}
